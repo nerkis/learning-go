2a30d0f3ac1ed108cb5e7052b921cd349b549452
Nicole Erkis <nerkis@urbn.com>
2018-06-26 11:37:21 -0400

    Add first pass solution for Quiz pt. 2

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -7,12 +7,14 @@ import (
 	"os"
 	"flag"
 	"strings"
+	"time"
 )
 
 func main() {
 	var count, correct int
 
-	file_name := flag.String("file", "problems.csv", "name of csv file in the format of 'question,answer'")
+	file_name := flag.String("file_name", "problems.csv", "name of csv file in the format of 'question,answer'")
+	time_limit := flag.Int("time_limit", 30, "time limit for the quiz")
 	flag.Parse()
 
 	csv_file, err := os.Open(*file_name)
@@ -27,16 +29,29 @@ func main() {
 		exit("Failed to parse file")
 	}
 
+	fmt.Println("Ready to start? Press enter to begin")
+	input_reader.ReadString('\n')
+
+	timer := time.NewTimer(time.Duration(*time_limit) * time.Second)
+	go func() {
+		<-timer.C
+		exit(fmt.Sprintf("\nTimer expired! You got %d questions correct and %d incorrect.\n", correct, (count - correct)))
+	}()
+
 	for _, line := range lines {
 		count++
 		fmt.Printf("Problem #%d: %s = ", count, line[0])
-		user_ans, _ := input_reader.ReadString('\n')
-		trimmed_answer := strings.TrimRight(user_ans, "\n")
-		if line[1] == strings.TrimSpace(trimmed_answer) {
+		ans, _ := input_reader.ReadString('\n')
+		if IsCorrect(line[1], ans) {
 			correct++
 		}
 	}
 	fmt.Printf("You got %d questions correct and %d incorrect.\n", correct, (count - correct))
+}
+
+func IsCorrect(expected, ans string) bool {
+	ans = strings.ToLower(strings.TrimSpace(strings.TrimRight(ans, "\n")))
+	return strings.ToLower(expected) == ans
 }
 
 func exit(msg string) {
